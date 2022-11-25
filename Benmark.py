dfdedfb31862d0f71337d6351b5e979aa38fbd95
Trương Đặng Văn Linh <vanlinh.1942003@gmail.com>
2022-11-25 23:43:47 +0700

    Update

--- Benmark.py
+++ Benmark.py
@@ -16,7 +16,7 @@ CFG = {
     #'model_arch': 'coat_tiny', #OK (Opset = 10)
     #'model_arch': 'gmlp_s16_224', #OK (Opset = 11)
     #'model_arch': 'inception_v4', #OK (Opset = 11)
-    'model_arch': 'resnet50', #OK (Opset = 11)
+    #'model_arch': 'resnet50', #OK (Opset = 11)
     #'model_arch': 'mixer_b16_224_in21k', #OK (Opset = 11)
     #'model_arch': 'deit_base_patch16_224', #OK (Opset = 11)
     #'model_arch': 'vit_base_patch16_224', #OK (Opset = 11)
@@ -26,11 +26,11 @@ CFG = {
     'batch_size': 1
 }
 
-def CreatTestBatch():
-    ImgPATH='./Dataset/test_images/Image/test4.png'
+def CreatTestBatch(num):
+    ImgPATH=f'./Dataset/test_images/Image/test{num}.png'
     img = resize(io.imread(ImgPATH), (224, 224))
     input_batch = np.array(np.repeat(np.expand_dims(np.array(img, dtype=np.float32), axis=0), CFG['batch_size'], axis=0), dtype=np.float32)
-    print(f'Shape: {input_batch.shape}')
+    #print(f'Shape: {input_batch.shape}')
     return input_batch
 
 def preprocess_image(img):
@@ -61,28 +61,35 @@ if __name__ == '__main__':
     print(f'Model: {CFG["model_arch"]}')
     print('Load model successfull!')
 
-    print('\nAllocating input and output memory...')
-    output = np.empty([CFG['batch_size'], 1000], dtype = np.float16) 
+    NumImg = 8
+    Avg = 0.0
+    for num in range (1, NumImg+1):
+        print(f'Allocating input and output memory for image {num}: ')
+        output = np.empty([CFG['batch_size'], 1000], dtype = np.float16) 
 
-    InputBatch = CreatTestBatch()
-    d_input = cuda.mem_alloc(1 * InputBatch.nbytes)
-    d_output = cuda.mem_alloc(1 * output.nbytes)
+        InputBatch = CreatTestBatch(num)
+        d_input = cuda.mem_alloc(1 * InputBatch.nbytes)
+        d_output = cuda.mem_alloc(1 * output.nbytes)
 
-    bindings = [int(d_input), int(d_output)]
-    stream = cuda.Stream()
-    print('Allocating sucessfully!')
+        bindings = [int(d_input), int(d_output)]
+        stream = cuda.Stream()
+        print('Allocating sucessfully!')
 
-    print('\nStart validating...')
-    Avg = 0
-    preprocessed_images = np.array([preprocess_image(image) for image in InputBatch])
+        print(f'\nStart validating image {num}: ')
+        preprocessed_images = np.array([preprocess_image(image) for image in InputBatch])
 
-    start = timeit.default_timer()
-    pred = predict(preprocessed_images)
-    print(pred)
-    stop = timeit.default_timer()
-    Avg += (stop - start)
-    print(f'Time: {Avg}')
-    print('Validating successfull!')
+        start = timeit.default_timer()
+        pred = predict(preprocessed_images)
+        stop = timeit.default_timer()
+        Avg += (stop - start) / NumImg 
+        print(f'Time for image {num}: {stop-start} second')
+        print('---------------------------')
+
+    f = open("Benmark.txt", "a")
+    s = f'Average validating time per image of {CFG["model_arch"]}: {str(Avg)} second\n'
+    print(s)
+    f.write(s)
+    f.close()
 
     '''
     print('Verify our TensorRT output is still accurate...')
