5bb49985117fd5afded6b50f7693c319c4998de6
Trương Đặng Văn Linh <vanlinh.1942003@gmail.com>
2022-12-08 02:49:49 +0700

    Config Model Resnet

--- Resnet.py
+++ Resnet.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-import timm
 
 def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
