44615061256ac3f1667f3ee0114feb66fdc58adb
Trương Đặng Văn Linh <88036898+MurazeKrinas@users.noreply.github.com>
2022-12-13 21:19:44 +0700

    Update Benchmark.py

--- Benchmark.py
+++ Benchmark.py
@@ -8,26 +8,26 @@ import numpy as np
 from skimage import io
 from skimage.transform import resize
 
-CFG = {
-    #'model_arch': 'tf_efficientnet_b4', #OK (Just ONNX, Opset = 11)
-    #'model_arch': 'convit_tiny', #OK (Just ONNX, Opset = 11)
-    #'model_arch': 'cait_s24_224', #OK (Opset = 11)
-    #'model_arch': 'coat_tiny', #OK (Opset = 10)
-    #'model_arch': 'gmlp_s16_224', #OK (Opset = 11)
-    #'model_arch': 'inception_v4', #OK (Opset = 11)
-    #'model_arch': 'resnet50', #OK (Opset = 11)
-    #'model_arch': 'mixer_b16_224_in21k', #OK (Opset = 11)
-    #'model_arch': 'deit_base_patch16_224', #OK (Opset = 11)
-    #'model_arch': 'vit_base_patch16_224', #OK (Opset = 11)
-    #'model_arch': 'resmlp_12_224', #ERROR: Operator addcmul
+Model = {
+    #'arch': 'tf_efficientnet_b4', #OK (Just ONNX, Opset = 11)
+    #'arch': 'convit_tiny', #OK (Just ONNX, Opset = 11)
+    #'arch': 'cait_s24_224', #OK (Opset = 11)
+    #'arch': 'coat_tiny', #OK (Opset = 10)
+    #'arch': 'gmlp_s16_224', #OK (Opset = 11)
+    #'arch': 'inception_v4', #OK (Opset = 11)
+    #'arch': 'resnet50', #OK (Opset = 11)
+    #'arch': 'mixer_b16_224_in21k', #OK (Opset = 11)
+    #'arch': 'deit_base_patch16_224', #OK (Opset = 11)
+    #'arch': 'vit_base_patch16_224', #OK (Opset = 11)
+    #'arch': 'resmlp_12_224', #ERROR: Operator addcmul
 
-    #'model_arch': 'Resnet50',
-    #'model_arch': 'Resnet18',
-    #'model_arch': 'Resnet10',
-    #'model_arch': 'Resnet8_V1',
-    #'model_arch': 'Resnet8_V2',
-    #'model_arch': 'Resnet8_V3',
-    #'model_arch': 'Resnet8_V4',
+    #'arch': 'Resnet50',
+    #'arch': 'Resnet18',
+    #'arch': 'Resnet10',
+    #'arch': 'Resnet8_V1',
+    #'arch': 'Resnet8_V2',
+    #'arch': 'Resnet8_V3',
+    #'arch': 'Resnet8_V4',
     'device': 'cuda:0',
     'batch_size': 4
 }
@@ -35,7 +35,7 @@ CFG = {
 def CreatTestBatch(num):
     ImgPATH=f'./Dataset/test_images/Image/test{num}.png'
     img = resize(io.imread(ImgPATH), (224, 224))
-    input_batch = np.array(np.repeat(np.expand_dims(np.array(img, dtype=np.float16), axis=0), CFG['batch_size'], axis=0), dtype=np.float16)
+    input_batch = np.array(np.repeat(np.expand_dims(np.array(img, dtype=np.float16), axis=0), Model['batch_size'], axis=0), dtype=np.float16)
     #print(f'Shape: {input_batch.shape}')
     return input_batch
 
@@ -58,20 +58,20 @@ def predict(batch): # result gets copied into output
 
 if __name__ == '__main__':
     print('Start loading model...')
-    PATH = f'./TRTModels/{CFG["model_arch"]}.trt'
+    PATH = f'./TRTModels/{Model["arch"]}.trt'
     f = open(PATH, "rb")
     runtime = trt.Runtime(trt.Logger(trt.Logger.WARNING)) 
 
     model = runtime.deserialize_cuda_engine(f.read())
     context = model.create_execution_context()
-    print(f'Model: {CFG["model_arch"]}')
+    print(f'Model: {Model["arch"]}')
     print('Load model successfull!')
 
     NumImg = 10
     Avg = 0.0
     for num in range (1, NumImg+1):
         print(f'Allocating input and output memory for image {num}: ')
-        output = np.empty([CFG['batch_size'], 1000], dtype = np.int8) 
+        output = np.empty([Model['batch_size'], 1000], dtype = np.int8) 
 
         InputBatch = CreatTestBatch(num)
         d_input = cuda.mem_alloc(1 * InputBatch.nbytes)
@@ -93,7 +93,7 @@ if __name__ == '__main__':
         print('---------------------------')
 
     f = open("Benchmark.txt", "a")
-    s = f'Average validating time per image of {CFG["model_arch"]}.trt: {str(Avg)} second\n'
+    s = f'Average validating time per image of {Model["arch"]}.trt: {str(Avg)} second\n'
     print(s)
     f.write(s)
     f.close()
@@ -104,4 +104,3 @@ if __name__ == '__main__':
     print("Class | Probability (out of 1)")
     print(list(zip(indices, pred[0][indices])))
     '''
-    
