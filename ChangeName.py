1ab839d9739e9decc423a2cd47bcb7d01b8524af
Trương Đặng Văn Linh <88036898+MurazeKrinas@users.noreply.github.com>
2022-12-27 22:37:37 +0700

    Delete ChangeName.py

--- ChangeName.py
+++
@@ -1,9 +0,0 @@
-import os
-
-path = './Dataset/Images/test_images/'
-num = 0
-for filename in os.listdir(path):
-    sour = path + filename
-    des = path + f'test{num}.jpg'
-    os.rename(sour, des)
-    num += 1
