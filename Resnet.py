ee29e3ae6e01a04edb4b06431a97d9f3a7a4ee3c
Trương Đặng Văn Linh <88036898+MurazeKrinas@users.noreply.github.com>
2023-01-11 00:35:52 +0700

    Update Resnet.py

--- Resnet.py
+++ Resnet.py
@@ -23,6 +23,7 @@ class BasicBlock(nn.Module):
         self.bn2 = norm_layer(planes)
         self.downsample = downsample
         self.stride = stride
+        self.drop15 = nn.Dropout(0.15)
 
     def forward(self, x):
         identity = x
@@ -30,7 +31,8 @@ class BasicBlock(nn.Module):
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
-
+        out = self.drop15(out)
+        
         out = self.conv2(out)
         out = self.bn2(out)
 
@@ -39,50 +41,6 @@ class BasicBlock(nn.Module):
 
         out += identity
         out = self.relu(out)
-
-        return out
-
-class Bottleneck(nn.Module):
-
-    expansion = 4
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
-                 base_width=64, dilation=1, norm_layer=None):
-        super(Bottleneck, self).__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        width = int(planes * (base_width / 64.)) * groups
-        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
-        self.conv1 = conv1x1(inplanes, width)
-        self.bn1 = norm_layer(width)
-        self.conv2 = conv3x3(width, width, stride, groups, dilation)
-        self.bn2 = norm_layer(width)
-        self.conv3 = conv1x1(width, planes * self.expansion)
-        self.bn3 = norm_layer(planes * self.expansion)
-        self.relu = nn.ReLU(inplace=True)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        identity = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu(out)
-
-        out = self.conv3(out)
-        out = self.bn3(out)
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
-        out = self.relu(out)
-
         return out
 
 class ResNet(nn.Module):
@@ -104,7 +62,7 @@ class ResNet(nn.Module):
         self.bn1 = norm_layer(self.inplanes)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.drop = nn.Dropout(0.3)
+        self.drop30 = nn.Dropout(0.3)
 
         self.layer1 = self.layer2 = self.layer3 = self.layer4 = None
         if (layers[0] != 0):
@@ -158,6 +116,8 @@ class ResNet(nn.Module):
         x = self.bn1(x)
         x = self.relu(x)
         x = self.maxpool(x)
+        x = self.drop30(x)
+        
 
         if (self.layer1 != None):
             x = self.layer1(x)
@@ -170,7 +130,7 @@ class ResNet(nn.Module):
 
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
-        x = self.drop(x)
+        x = self.drop30(x)
         x = self.fc(x)
 
         return x
@@ -180,12 +140,7 @@ class ResNet(nn.Module):
 
 def Resnet(Arch, **kwargs):
     print(f'Model: {Arch}')
-    if (Arch == 'Resnet50'):
-        Layer = [3, 4, 6, 3]
-        return ResNet(Bottleneck, Layer, **kwargs)
-    elif (Arch == 'Resnet18'):
-        Layer = [2, 2, 2, 2]
-    elif (Arch == 'Resnet10'):
+    if (Arch == 'Resnet10'):
         Layer = [1, 1, 1, 1]
     elif (Arch == 'Resnet8_V1'):
         Layer = [0, 1, 1, 1]
@@ -197,4 +152,3 @@ def Resnet(Arch, **kwargs):
         Layer = [1, 1, 1, 0]
     
     return ResNet(BasicBlock, Layer, **kwargs)
-
